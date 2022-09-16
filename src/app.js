4ce463852bbba4d5687ae25f533c15534a45b17e
Justin Moore <kaytin143@gmail.com>
2022-09-16 04:08:26 -0700

    Added webpacj and plyr

--- src/app.js
+++ src/app.js
@@ -1,21 +1,49 @@
-const animeVid = document.getElementById('anime-vid')
 const h2 = document.querySelector('h2')
 const hero = document.querySelector('.synopsis')
 const animeImg = document.querySelector('.anime-img')
 const buttons = document.querySelectorAll('button');
 const episodes = document.querySelector('.episodes')
 
-export default function loadSpy() {
+
 
 
 async function getAnime(episode) {
     var grabIt = await fetch(`https://gogoanime.herokuapp.com/vidcdn/watch/spy-x-family-episode-${episode}`,
     { mode: "cors" });
     var dataRet = await grabIt.json();
-    console.log(dataRet)
-    var vid = dataRet.Referer
-    animeVid.src = vid;
-}
+    console.log(dataRet.sources[0]["file"])
+    var vid = dataRet.sources[0]["file"]
+      const source = vid;
+
+      const video = document.querySelector('video');
+      
+      // For more options see: https://github.com/sampotts/plyr/#options
+      // captions.update is required for captions to work with hls.js
+      const player = new Plyr(video, {captions: {active: true, update: true, language: 'en'}});
+      
+      player.quality = '1080'
+      if (!Hls.isSupported()) {
+        video.src = source;
+      } else {
+        // For more Hls.js options, see https://github.com/dailymotion/hls.js
+        const hls = new Hls();
+        hls.loadSource(source);
+        hls.attachMedia(video);
+        window.hls = hls;
+        
+        // Handle changing captions
+        player.on('languagechange', () => {
+          // Caption support is still flaky. See: https://github.com/sampotts/plyr/issues/994
+          setTimeout(() => hls.subtitleTrack = player.currentTrack, 50);
+        });
+      }
+    
+          // Expose player so it can be used from the console
+      window.player = player;
+    }
+
+
+
 
 
 async function getAnimeDetails() {
@@ -46,4 +74,3 @@ for (var i = 0; i < btns.length; i++) {
   this.disabled = "disabled"
     });
   }
-}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import loadSpy from "./app";
-const test = document.getElementById('test')
-test.addEventListener('click', () => {
-    window.open('/dist/homePage.html', '_self')
-})
-loadSpy();
+// import loadSpy from "./app";
+// // const test = document.getElementById('test')
+// // test.addEventListener('click', () => {
+// //     window.open('/dist/homePage.html', '_self')
+// // })
+// loadSpy();
